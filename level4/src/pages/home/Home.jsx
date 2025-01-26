5c600941d5be6a6ee69da4b49514587baef85d0c
yousrik20 <yousri.k.e13@gmail.com>
2025-01-26 17:45:26 +0100

    lesson 13

--- level4/src/pages/home/Home.jsx
+++ level4/src/pages/home/Home.jsx
@@ -1,8 +1,75 @@
-import './Home.css';
-import React from 'react'
+import { Box, Paper, Typography, IconButton } from "@mui/material";
+import "./Home.css";
+import React from "react";
+import { Close } from "@mui/icons-material";
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <Box>
+      <Paper
+        sx={{
+          width: "366px",
+          display: "flex",
+          justifyContent: "space-between",
+          mt: "22px",
+          pt: "27px",
+          pb: "7px",
+          position: "relative",
+        }}
+      >
+        <Typography sx={{ ml: "16px", fontSize: "1.3em" }} variant="h6">
+          GYM
+        </Typography>
+        <Typography
+          sx={{
+            mr: "33px",
+            fontWeight: "500",
+            fontSize: "1.4em",
+            opacity: "0.8",
+          }}
+          variant="h6"
+        >
+          $100
+        </Typography>
+        <IconButton
+          aria-label=""
+          sx={{ position: "absolute", top: "0px", right: "0px" }}
+        >
+          <Close sx={{ fontSize: "20px" }} />
+        </IconButton>
+      </Paper>
+      <Paper
+        sx={{
+          width: "366px",
+          display: "flex",
+          justifyContent: "space-between",
+          mt: "22px",
+          pt: "27px",
+          pb: "7px",
+          position: "relative",
+        }}
+      >
+        <Typography sx={{ ml: "16px", fontSize: "1.3em" }} variant="h6">
+          GYM
+        </Typography>
+        <Typography
+          sx={{
+            mr: "33px",
+            fontWeight: "500",
+            fontSize: "1.4em",
+            opacity: "0.8",
+          }}
+          variant="h6"
+        >
+          $100
+        </Typography>
+        <IconButton
+          aria-label=""
+          sx={{ position: "absolute", top: "0px", right: "0px" }}
+        >
+          <Close sx={{ fontSize: "20px" }} />
+        </IconButton>
+      </Paper>
+    </Box>
+  );
 }
